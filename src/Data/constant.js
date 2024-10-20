fdd0aa45aedb0333674701c6f0b3e11cfdc4457b
utkarshsinghakash <146868670+utkarshsinghakash@users.noreply.github.com>
2024-10-21 02:48:22 +0530

    resume

--- src/Data/constant.js
+++ src/Data/constant.js
@@ -16,7 +16,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/utkarshsinghakash",
   resume:
-    "https://drive.google.com/file/d/1_uSIinl7vDwUV7brgwhvx7h80vx9H-73/view?usp=sharing",
+    "https://drive.google.com/file/d/1Z199vYrtfqRWlW82CD-RypAh1bGzpJZD/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/utkarsh-singh-b9574b262/",
 
   insta: "https://www.instagram.com/itz_utkarshsinghh/?hl=en",
